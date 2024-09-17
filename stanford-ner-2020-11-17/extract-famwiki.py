37410b8b1fc7f1785672fb8d4a98ac0a747d0bbe
Sabih_Shah <113444135+SabihShah@users.noreply.github.com>
2024-09-17 17:06:45 +0500

    Update and rename extract-fanwiki.py to extract-famwiki.py

--- stanford-ner-2020-11-17/extract-famwiki.py
+++ stanford-ner-2020-11-17/extract-famwiki.py
@@ -1,9 +1,7 @@
 import fandom
 
-# Set the wiki to "jojo" (JoJo's Bizarre Adventure)
 fandom.set_wiki("jojo")
 
-# Search for "Hol Horse" to get the precise page title
 search_results = fandom.search("Hol Horse")
 print(search_results)
 
@@ -11,15 +9,11 @@ if search_results:
     text = []
     for result in search_results:
         try:
-            # Get the first search result's title
             page_title = result[0]
-            # Load the page using the page title
             page = fandom.page(title=page_title)
-            # Get the content of the page
             content = page.content
 
             print(content.keys())
-            # Save the content to fanwiki.txt
             text.append(content['content'])
         except:
             continue
